efc21dc0d32ffade8d77bdb6ad1c77cfe43f98a6
Juldan Umbu Riada <juldanriada@gmail.com>
2022-09-26 10:16:49 +0700

    CommentIsDevelopment

--- Program.cs
+++ Program.cs
@@ -50,11 +50,11 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     // app.UseSwagger();
+//     // app.UseSwaggerUI();
+// }
 
 // app.UseHttpsRedirection();
 
